0274dca961038c7336526385e42ac711a25868a0
Monroe <monroedrysdale@gmail.com>
2022-11-29 18:37:25 -0800

    Add button and store editText string when button is clicked

--- app/src/main/java/com/bignerdranch/android/twitter_downloader/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/twitter_downloader/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.twitter_downloader
 
 import android.os.Bundle
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity
 import com.bignerdranch.android.twitter_downloader.api.getTweetJSONByID
 import kotlinx.coroutines.runBlocking
@@ -9,14 +12,30 @@ import kotlinx.coroutines.runBlocking
 private const val TAG = "Main Activity"
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button: Button
+    lateinit var editText: EditText
+    lateinit var string: String //use this to store the data of the EditText
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.button)
+        editText = findViewById(R.id.textInputEditText)
+        textView = findViewById(R.id.textView)
+
+        button.setOnClickListener {
+            string = editText.text.toString()
+            textView.text = string
+        }
+
         test()
     }
 
     fun test() = runBlocking() {
         getTweetJSONByID("1591434056323203072")
+
     }
 
 
